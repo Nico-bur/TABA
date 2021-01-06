9d28d5adbe19c859a38d7a6fdfc6bc19a5dc9c4c
Nicoletta Burduja <nicoburduja@Nicolettas-MBP.fritz.box>
2021-01-06 21:55:01 +0000

    first commit

---
+++ FindComputerWordsGame.java
@@ -0,0 +1,9 @@
+//instantiable class to compute the points for the words provided by the user in the Application
+public class FindComputerWordsGame{
+  //vars
+  private   
+  //constructor
+  //set
+  //compute
+  //get
+}
---
+++ FindComputerWordsGameApp.java
@@ -0,0 +1,12 @@
+//App class to allow 2 users to input words and output the points they get
+public class FindComputerWordsGameApp{
+  public static void main(String[] args){
+    //vars
+    //objects
+    //input
+    
+    //compute
+    //get
+    //output
+  }
+}
